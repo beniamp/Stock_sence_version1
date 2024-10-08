c08ab4eefbbbff759a2a4e8b63d7311bcf30e23f
beniamp <102410523+beniamp@users.noreply.github.com>
2024-10-08 17:17:14 +0330

    Update stock_sence_program_ver1.py

--- stock_sence_program_ver1.py
+++ stock_sence_program_ver1.py
@@ -193,7 +193,7 @@ with col3:
 
 
 # Create 3 columns with empty space on both sides for centering the buttons
-col1, col2, col3, col4, col5, col6 = st.columns([2, 2, 2, 2, 2, 2])
+col1, col2, col3, col4, col5, col6 = st.columns([1, 1, 1, 1, 1, 1])
 
 with col1:
     butt1 = st.button('1 Day Selection')
