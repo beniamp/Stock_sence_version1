3d994e12134eab0ec589b55d9eb94386802e2e83
beniamp <102410523+beniamp@users.noreply.github.com>
2024-10-08 12:31:05 +0330

    Update stock_sence_program_ver1.py

--- stock_sence_program_ver1.py
+++ stock_sence_program_ver1.py
@@ -125,7 +125,7 @@ total_quantity_overall = df['total_quantity'].sum()
 # ----- SECTION 5: displayment to selection widget on DLP, DLPC, and stores unique values 
 
 # Select box widget values, in order; DLP, store, DLPC
-product_list = ['All products'] + merged_df['category'].unique().tolist()
+product_list = ['All products'] + merged_df['DLP'].unique().tolist()
 category_list = ['All categories'] + merged_df['category'].unique().tolist()
 
 col01, col02 = st.columns(2)
